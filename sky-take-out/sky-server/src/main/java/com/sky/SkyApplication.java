eda6b2e640f7c909cb0de1360b95eb7d8a3ba9a1
zhouhui0802 <1741484908@qq.com>
2026-04-10 15:54:07 +0800

    WebSocke进行长连接

--- sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement //开启注解方式的事务管理 没有它，@Transactional 写了也白写，事务不生效！ 在 SpringBoot 里可写可不写，写了也不报错
 @Slf4j
 @EnableCaching
+@EnableScheduling  //开启定时任务的注解  这个注解在 spring-context中，也在Spring-boot-starter
 public class SkyApplication
 {
     public static void main( String[] args )
---
+++ sky-take-out/sky-server/src/main/java/com/sky/config/WebSocketConfiguration.java
@@ -0,0 +1,24 @@
+package com.sky.config;
+
+
+/**
+ * @author zhouhui
+ * @version 1.0
+ * @description TODO
+ * @date 2026/4/10 15:01
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * WebSocket配置类，用于注册WebSocket的Bean
+ */
+@Configuration
+public class WebSocketConfiguration {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
